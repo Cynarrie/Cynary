f9d627b45f7ab41da7b037afcd3aed9e3ddb9734
cynary <cynarrie@gmail.com>
2015-02-27 07:43:12 +0000

    First push

---
+++ server.js
@@ -0,0 +1,25 @@
+var port = (process.env.PORT || process.env.VMC_APP_PORT);
+var host = (process.env.IP || process.env.VCAP_APP_HOST);
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(express.static('./web'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/jark', function(req, res){
+        
+}
+);
+
+app.post('/jarkmanager', function(req, res){
+        switch (req.body.q0) {
+            case "Insert_Interface":
+              res.send(req.body.q0);
+            break;
+        }
+    }
+);
+
+app.listen(port);
